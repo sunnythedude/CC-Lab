c73ad35bab67c38c37207ed9f81c13972b77a46a
sunnythedude <sunnythedude@users.noreply.github.com>
2016-10-02 17:09:30 -0400

    pressure sensor assignment
    Sodered resistor to A0

---
+++ pressure_sensor_mine.ino
@@ -0,0 +1,22 @@
+int fsrPin = A0;
+int ledPin = 4;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(fsrPin, INPUT);
+pinMode(ledPin, OUTPUT); 
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int fsrVal = analogRead(fsrPin);
+
+Serial.println(fsrVal);
+ 
+if(fsrVal > 300) {
+  digitalWrite(ledPin, HIGH); 
+} else {
+  digitalWrite(ledPin, LOW);
+} 
+}
